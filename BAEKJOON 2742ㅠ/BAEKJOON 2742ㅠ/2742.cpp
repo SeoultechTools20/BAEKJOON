0bd1aba73769a80ed279ddba02e9fb1e9b0753c4
16101393@officestu.seoultech.ac.kr <16101393@officestu.seoultech.ac.kr>
2020-04-12 18:55:55 +0900

    백준 2742 풀음.

---
+++ BAEKJOON 2742ㅠ/BAEKJOON 2742ㅠ/2742.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	for (int i = n; i > 0; i--)
+	{
+		cout << i << "\n";
+	}
+}
